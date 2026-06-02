0ffec30fdfbbe84fab4e2e0bc7c692a601137cee
shaw <dashingwx@outlook.com>
2026-06-02 16:09:52 +0800

    更新了非线性优化内容

---
+++ week1_filter_slam/01_bayes_filter_handmade.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+// 使用标准命名空间，简化代码编写
+using namespace std;
+
+// ===================== 核心参数 =====================
+const int n = 5; // 走廊格子数量
+// 地图：true=门，false=非门（对应 0/2/4 是门）]
+vector<bool> corridor = {true, false, true, false, true};
+
+// 
+
